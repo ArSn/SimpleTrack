6a98b52458b589b3927a4706e57ae6cb35376b03
johann <johann@iteske.de>
2021-10-25 22:05:08 +0200

    extended DockerFile and added some structure

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
 
+namespace application;
+
+use infrastructure\PDORepository;
+
+include("Application/UserService.php");
+
 date_default_timezone_set('Europe/Berlin');
 
 echo "<p>Welcome to our awesome application.</p>";
 
 echo "<p>It is now " . date("Y-m-d H:i:s") . "</p>";
+
+$repository = new PDORepository();
+$userService = new UserService($repository);
+
+echo $userService->get($userId);
+
+
+
+/*
+$var = new config();
+$var->init_database();
+*/
